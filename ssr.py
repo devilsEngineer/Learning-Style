82542386ce1e21331635679cefeffb7b1621ef10
Ananthu S Kuttattu <anandhus.kuttattu@gmail.com>
2019-04-07 13:54:13 +0530

    combination of learning style
    Each combination is a result of analyzing similar cluster

--- ssr.py
+++ ssr.py
@@ -31,7 +31,7 @@ def preprocess_features(X):
 X_all=preprocess_features(df)
 
 from sklearn.cluster import KMeans
-n_clusters=7
+n_clusters=21
 kmeans = KMeans(n_clusters)
 kmeans.fit(X_all)
 centroids =(kmeans.cluster_centers_)
@@ -58,16 +58,69 @@ print(confusion_matrix(y_test,pred))
 print("Classification Matrix :")
 print(classification_report(y_test, pred))
 
+y_train=y_train.to_frame()
+y_test=y_test.to_frame()
+bigdata=pd.DataFrame()
+bigdata['Cluster'] = (x_train['Cluster'])
+bigdata['Learner'] = (y_train['Learner'])
 
-from scipy.spatial import distance
+"""Finding learning style frequency in each cluster"""
+clusterFreq=[]
+for i in range(0,n_clusters):
+    temp=[0,0,0]
+    for index,j in bigdata.iterrows():
+        if(i==j['Cluster']):
+            temp[j['Learner']]+=1
+    clusterFreq.append(temp)       
+#print(clusterFreq)
+
+"""Finding the dominant learning style in each cluster"""
+clusterDominance={}
+ind=0
+for i in clusterFreq:
+    clusterDominance[ind]=i.index(max(i))
+    ind+=1
+#print(clusterDominance)
+#from scipy.spatial import distance
+    
+"""Finding adjusted rand index value with respect of each cluster centroids"""
+from sklearn.metrics.cluster import adjusted_rand_score
 cluster_distance=[]
 for i in range(0,len(centroids)):
     temp=[]
     for c in centroids:
-        temp.append(distance.euclidean(centroids[i],c))
+        temp.append(adjusted_rand_score(centroids[i],c))
+        #temp.append(distance.euclidean(centroids[i],c))
     cluster_distance.append(temp)
+
+print("Adjusted Rand index :")
 print(cluster_distance)
 
+threshold=1.0
+
+"""Function to find indices of a given element"""
+def indices(lst, element):
+    result = []
+    offset = -1
+    while True:
+        try:
+            offset = lst.index(element, offset+1)
+        except ValueError:
+            return result
+        result.append(offset)
+
+"""Finding combination of learning style by combining dominance of similiar clusters""" 
+mapLearn={'0':'A','1':'V','2':'K'}
+finalLearning=[]
+for clust in range(0,len(cluster_distance)):
+    thresholdIndex=indices(cluster_distance[clust],threshold)
+    finalLearning.append(mapLearn[str(clusterDominance[i])])
+    for key in thresholdIndex:
+        if(mapLearn[str(clusterDominance[key])] not in finalLearning[clust]):
+            finalLearning[clust]= finalLearning[clust]+mapLearn[str(clusterDominance[key])]
+
+print("Cluster Learning Style Combination :")      
+print(finalLearning)
 
 
 
